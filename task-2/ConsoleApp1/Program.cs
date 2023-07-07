3860ad91db66701060d2fa4236382fa347869009
ElenaAlena <elena.pigaleva@gmail.com>
2023-07-08 00:36:58 +0300

    Task-2. Part 2,a

--- task-2/ConsoleApp1/Program.cs
+++ task-2/ConsoleApp1/Program.cs
@@ -39,13 +39,15 @@ namespace ConsoleApp1
             // приведение типов
             double a = 4.0;
             decimal b = (decimal)a;
-            Console.WriteLine(b);
+            Console.WriteLine(b);*/
+
+            /*int a1 = 10;
+            float _a1 = a1;
+            Console.WriteLine(a1);//10
+                                  // double b1 = (double)a1;
+                                  // Console.WriteLine(a1);
+
             
-            int a1 = 10;
-            double b1 = (double)a1;
-            Console.WriteLine(a1);
-
-
             float a2 = 65.35f;
             double b2 = (double)a2;
             Console.WriteLine(a2);
@@ -65,6 +67,9 @@ namespace ConsoleApp1
             Console.WriteLine(s_ushort2);
             Console.WriteLine("Введите число типа int");
             var x = Convert.ToInt32(Console.ReadLine());
+            var _intA = 10; // 10
+            var _charA = 'a';// a
+            var __stringA = "str"; // str
             var intX = new[] {1, 5, 15 };
             var stringX = new[] { "Hello", "World", "7"};
             int intXnIndex2 = intX[2];
@@ -73,7 +78,9 @@ namespace ConsoleApp1
 
 
 
-            string? stringNullable = null;
+            /* string ? stringNullable = null;
+
+            Console.WriteLine(stringNullable);
             void IsNull(string? obj)
             {
                 if (obj == null)
@@ -90,13 +97,64 @@ namespace ConsoleApp1
 
 
             // test = 22; - за переменной test закреплен тип string, который не может измениться до конца существования переменной test на другой тип
+            int? val = null;
+            Console.WriteLine(val);*/
 
-            Console.ReadLine();
 
-        }
-        
 
+            // строки
+            ///а
+            string str1 = "AAAbbbCCC";
+            string str2 = "ddEEff";
+            string str3 = "GHK";
+           
+            // сцепление
+            string str4 = str1 + str2 + str3;
+            Console.WriteLine(str1 + str2 + str3);
+            Console.WriteLine(str4);
+            
             
 
-    }   
+            /// копирование
+            string str5 = str4;
+            Console.WriteLine("str 5 = " + str5);
+            Console.WriteLine("str 4 = " + str4);
+            str4 = "vasav";
+            Console.WriteLine("str 4 = " + str4);
+            Console.WriteLine("str 5 = " + str5);
+
+            //метод CopyTo: копирует часть строки, начиная с определенного индекса в массив
+            char[] arr = new char[str4.Length];
+            str4.CopyTo(0, arr, 0, str4.Length);
+            Console.WriteLine();
+            foreach (char c in arr) { Console.Write(c); }
+
+            // метод Insert: вставляет в строку подстроку
+            string str6 = str4.Insert(3, str5);
+            Console.WriteLine();
+            Console.WriteLine(str6);
+
+            // метод Substring: извлекает из строки подстроку, начиная с указанной позиции
+            Console.WriteLine("Введите строку");
+            string str7 = Console.ReadLine();
+            string str8 = "";
+            Console.WriteLine("Введите символ, с которого нужно скопировать строку");
+            int indexStart = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Введите количество символов для копирования");
+            int countIndex = Int32.Parse(Console.ReadLine());
+            if (indexStart > str7.Length || countIndex > str7.Length || indexStart > countIndex)
+                Console.WriteLine("ERROR");
+            else
+            Console.WriteLine(str8 =  str7.Substring(indexStart-1, countIndex-1));
+            
+            
+
+
+        }
+
+
+
+
+
+    }
 }
