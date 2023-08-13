4d61efc0e7884fc29a96b5fe6a3d0cf098fc648c
AlexNovik <xwordik@gmail.com>
2023-08-13 23:25:38 +0300

    Task 3-4

---
+++ task-3/continue/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------- Homework 3 ----------");
+            Console.WriteLine("--------- Task 2 ----------");
+            Console.WriteLine("--------- a ----------");
+            void printList<T>(List<T> list)
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    Console.Write(list[i].ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+            List<int> listInt = new List<int>() { 1, 2, 3 };
+            printList(listInt);
+            listInt.Add(4);
+            printList(listInt);
+
+
+            Console.WriteLine(listInt[0]);
+
+            listInt.Clear();
+            printList(listInt);
+
+            Console.WriteLine("stack");
+
+            void printStack(Stack<int> stack)
+            {
+                foreach (var item in stack)
+                {
+                    Console.Write(item.ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Stack<int> stackInt = new Stack<int>();
+            stackInt.Push(5);
+            stackInt.Push(6);
+            stackInt.Push(7);
+            printStack(stackInt);
+
+            List<int> listInt2 = new List<int> { 1, 2, 3 };
+            Stack<int> stackInt2 = new Stack<int>(listInt2);
+            printStack(stackInt2);
+            Console.WriteLine("pop: " + stackInt.Pop());
+            printStack(stackInt);
+
+            Console.WriteLine("peek: " + stackInt2.Peek());
+            printStack(stackInt2);
+            stackInt.Clear();
+            printStack(stackInt);
+
+            Console.WriteLine("Dictionary");
+            void printDictionary(Dictionary<int, string> dictionary)
+            {
+                foreach (var item in dictionary)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+                Console.WriteLine();
+            }
+            Dictionary<int, string> dictionary1 = new Dictionary<int, string>() { { 1, "One" }, { 2, "Two" }, { 3, "Three" } };
+            printDictionary(dictionary1);
+            Console.WriteLine(dictionary1[1]);
+            dictionary1.Add(5, "five");
+            printDictionary(dictionary1);
+            dictionary1.Clear();
+            printDictionary(dictionary1);
+
+            Console.WriteLine("--------- b ----------");
+            List<List<int>> matrix = new List<List<int>>() { new List<int> { 1, 2, 3 }, new List<int> { 4, 5, 6 }, new List<int> { 7, 8, 9 } };
+            matrix.Add(new List<int> { 2, 5, 8 });
+
+            foreach (var item in matrix)
+            {
+                printList(item);
+            }
+
+            Console.WriteLine("--------- Task 3 ----------");
+            Console.WriteLine("--------- a ----------");
+
+            string[] months = { "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" };
+            IEnumerable<string> lengthMonthN = from month in months where month.Length == 5 select month;
+            void print(IEnumerable<string> data)
+            {
+                foreach (var item in data)
+                {
+                    Console.Write(item + "  ");
+                }
+                Console.WriteLine();
+            }
+            print(lengthMonthN);
+
+            Dictionary<string, List<string>> seasonOfYear = new Dictionary<string, List<string>>() {{"winter", new List<string> { "january", "february", "december" } },
+                    {"spring", new List<string> { "march", "april", "may" } }, {"summer",  new List<string>{ "june", "july", "august" } }, {"autumn",new List<string>{ "september", "october", "november" } } };
+
+
+            
+
+            var winterSummerMonths = from season in seasonOfYear where season.Key == "winter" || season.Key == "summer" select season.Value;
+           
+            foreach (var item in winterSummerMonths)
+            {
+                printList(item);
+            }
+
+            var alphabetMonth = from month in months orderby month select month;
+            print(alphabetMonth);
+
+            var someMonts = from month in months where month.Contains("u") && month.Length > 3 select month;
+            var countSomeMonths = (from month in months where month.Contains("u") && month.Length > 3 select month).Count();
+            print(someMonts);
+            Console.WriteLine(countSomeMonths);
+            
+            Console.WriteLine("--------- b ----------");
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ task-4/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------- Homework 4 --------------");
+            Console.WriteLine("------------- Task 1 a --------------");
+
+            //"abd".ToUpper().ToArray();
+
+            void ToUpperCase(string str)
+            {
+               Console.WriteLine(str.ToUpper());
+            }
+
+            string RemoveExtraWhiteSpace(string str)
+            {
+                return str.Trim();
+            }
+
+            string RemoveNumber(string str)
+            {
+                string buffer = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ((char)str[i] < '0' || (char)str[i] > '9')
+                        buffer += str[i];
+                }
+                return buffer;
+            }
+
+
+            string RemoveSimbol(string str)
+            {
+                string buffer = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ((char)str[i] != '.' && (char)str[i] != ',' && (char)str[i] != '!' && (char)str[i] != '?')
+                        buffer += str[i];
+                }
+                return buffer;
+            }
+
+            string RemoveSimbol2(string str, char chDel, char chAdd)
+            {
+                string buffer = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ((char)str[i] == chDel)
+                    {
+                        buffer += chAdd;
+                    }
+                    else buffer += str[i];
+                }
+                return buffer;
+            }
+
+
+            var str1 = Console.ReadLine();
+
+
+            Console.WriteLine(RemoveSimbol2(str1, ' ', '*'));
+
+            string AddSimbol(string str)
+            {
+                string buffer = "";
+                for (int i = 0; i < str.Length - 1; i++)
+                {
+                    if ((char)str[i] != Char.ToUpper((char)str[i]) && (char)str[i + 1] == Char.ToUpper((char)str[i + 1]))
+                        buffer += " ";
+                    else buffer += str[i];
+                }
+                return buffer;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("------------- Task 1 b  --------------");
+            Console.WriteLine();
+
+
+            StrDeligate removeSimbol = RemoveSimbol2;
+            Console.WriteLine(removeSimbol(str1, 'e', 'a'));
+
+            void funk1(StrDeligate strD, Func<string, string> addS) 
+            {
+                string str = Console.ReadLine();
+                str = strD(str, 'e', 'a');
+                str = addS(str);
+                Console.WriteLine(str);
+            };
+            funk1(RemoveSimbol2, AddSimbol);
+
+            int strLength(string str) 
+            {
+                return str.Length;
+            };
+
+            bool strMore(int length)
+            {
+                if (length > 5)
+                { return true; }
+                else { return false; }
+            };
+            bool strLess(int length)
+            {
+                if (length < 5)
+                { return true; }
+                else { return false; }
+            };
+
+            void AddA (string str)
+            {
+                str += 'A';
+                Console.WriteLine(str);
+            };
+
+            void func2(Func<string, int> getLength, Predicate<int> checkStrMore, Predicate<int> checkStrLess,  Action<string> addSimbolA, Action<string> strUpCase) 
+            {
+                string str = Console.ReadLine();
+                var strLengh = getLength(str);
+                if (checkStrMore(strLengh))
+                { addSimbolA(str); }
+                else { strUpCase(str); }
+
+            };
+            Console.WriteLine("Вызов функции 2");
+            func2(strLength, strMore, strLess, AddA,ToUpperCase);
+
+                       
+            Console.WriteLine("------------- Task 2 a --------------");
+
+
+            Action<int, int> multLambda = (int x, int y) =>
+            {
+                Console.WriteLine($"{x * y}");
+            };
+           
+            Func<int,int, int> sumLambda = (int a, int b ) => a+ b;
+
+            Predicate<string> stringLtngthLambdaM = (string strL) =>
+            {
+                if (strL.Length > 10)
+                { return true; }
+                else { return false; }
+            };
+
+            Predicate<string> stringLtngthLambdaL = (string strL) =>
+            {
+                if (strL.Length < 10)
+                { return true; }
+                else { return false; }
+            };
+
+            Func<string, string> strAddA = (string str) => { return str + "A"; };
+
+            Func<string, string> strToLowLamb = (string str) => { return str.ToLower(); };
+
+            Func<string, string> strRemovBackSpace = (str) => 
+            {
+                string buffer = "";
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if ((char)str[i] != ' ' )
+                        buffer += str[i];
+                    else { buffer += ','; }
+                } 
+                return buffer;
+            };
+            strAddA(str1);
+
+            Console.ReadLine();
+
+
+
+        }           
+
+    }
+
+
+    delegate string StrDeligate(string str, char ch, char ch2);
+        
+}
