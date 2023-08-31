3dfcce76e87828b264342ed1478fc00d0919a879
AlexNovik <xwordik@gmail.com>
2023-09-01 00:30:04 +0300

    Homework-6

---
+++ task-6/Homework-6/Order.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    public class Order
+    {
+        int id;
+        string firstNameCustomer;
+        string lastNameCustomer;
+        string surnameCustomer;
+        string adressCustomer;
+        string productName;
+        string discriptionOfProduct;
+        double priceOfProduct;
+        int countProduct;
+
+        public int Id
+        {
+            get { return id; }
+        }
+        public string FirstNameCustomer
+        {
+            get { return firstNameCustomer; }
+            set { if (!string.IsNullOrEmpty(value)) firstNameCustomer = value; }
+        }
+        public string LastNameCustomer
+        {
+            get { return lastNameCustomer; }
+            set { if (!string.IsNullOrEmpty(value)) lastNameCustomer = value; }
+        }
+        public string SurnameCustomer
+        {
+            get => surnameCustomer;
+            set => surnameCustomer = value;
+        }
+        public string AdressCustomer
+        {
+            get => adressCustomer;
+            set { if (!string.IsNullOrEmpty(value)) adressCustomer = value; }
+        }
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        public string DiscriptionOfProduct
+        {
+            get { return discriptionOfProduct; }
+            set { discriptionOfProduct = value; }
+        }
+        public double PriceOfProduct
+        {
+            get => priceOfProduct;
+            set { if (value > 0) priceOfProduct = value; }
+        }
+        public int CountProduct
+        {
+            get { return countProduct; }
+            set { if (value > 0) countProduct = value; }
+        }
+        public Order(int id, string firstNameCustomer, string lastNameCustomer, string surnameCustomer, string adressCustomer, string productName, string discriptionOfProduct, double priceOfProduct, int countProduct)
+        {
+            this.id = id;
+            this.firstNameCustomer = firstNameCustomer;
+            this.lastNameCustomer = lastNameCustomer;
+            SurnameCustomer = surnameCustomer;
+            this.adressCustomer = adressCustomer;
+            this.productName = productName;
+            this.discriptionOfProduct = discriptionOfProduct;
+            this.priceOfProduct = priceOfProduct;
+            this.countProduct = countProduct;
+        }
+        public Order(string firstNfmeCustomer, string lastNameCustomer, string productName, double priceOfProduct)
+        {
+            this.firstNameCustomer = firstNfmeCustomer;
+            this.lastNameCustomer = lastNameCustomer;
+            this.productName = productName;
+            this.priceOfProduct = priceOfProduct;
+        }
+        public double SumProduct(double price, int count)
+        {
+            return price * count;
+        }
+
+    }
+}
---
+++ task-6/Homework-6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var order1 = new Order(1, "Иван", "Иванов", "Иванович", "г.Минск, ул. Красивая", "Молоко", "Брест-литовское, 3,6 %", 2.8, 3);
+            var order2 = new Order(2, "Александр", "Александров", "Александрович", "г.Минск, ул. Ясная", "сельдь", "Матиас", 5.8, 2);
+            var order3 = new Order("Сидр", "Сидоров", "огурец", 3);
+            Console.WriteLine(Equals(order1, order2)); // сравнение объектов
+            Console.WriteLine(Equals(order3, order2));
+            Console.WriteLine(order1.GetType()); // проверка типа созданного объекта
+            List<Order> orders = new List<Order>();
+            orders.Add(order1);
+            orders.Add(order2);
+            orders.Add(order3);
+            Console.WriteLine(order1.FirstNameCustomer + " " + order1.ProductName);
+            Order[] arrayOrders = new Order[] { order1, order2, order3 };
+            // Отсортировать массив по возрастанию полной стоимости заказа. 
+
+            Console.ReadLine();
+        }
+    }
+
+}
